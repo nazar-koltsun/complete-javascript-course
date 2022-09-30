863b74ce967f7c2d4038e2d2d5e4905e47fcb333
Nazar Koltsun <nazar-koltsun@ukr.net>
2022-09-30 22:58:24 +0300

    01-Fundamentals-Part-1 Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,12 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+let country = 'Ukraine';
+let continent = 'Europe';
+let population = 39700000;
 
-console.log(40 + 8 + 23 - 10);
+
+let isIsland = false;
+let language;
+
+console.log('isIsland', typeof isIsland);
+console.log('population', typeof population);
+console.log('country', typeof country);
+console.log('language', typeof language);
